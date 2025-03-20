c4840578bd01adf7b01ebfac643930e741a54ed8
Matan Danieli <matandanieli123@gmail.com>
2025-03-20 14:57:34 +0200

    Changes to Media - Folders repository and interfaces

---
+++ backend/media/Interfaces/Model-Interfaces/TV/tv-series-interface.ts
@@ -0,0 +1,17 @@
+export interface ITV {
+    backdrop_path: string,
+    id: number,
+    name: string,
+    original_name: string,
+    overview: string,
+    poster_path: string,
+    media_type: string,
+    adult: boolean,
+    original_language: string,
+    genre_ids:number[]
+    popularity: number,
+    first_air_date: string,
+    vote_average: number,
+    vote_count: number,
+    origin_country: string[]
+}
---
+++ backend/media/Interfaces/media-repository-interface.ts
@@ -0,0 +1,13 @@
+import { IFullMovie } from "./Model-Interfaces/MOVIE/full-movie-interface";
+import { IFullTVSeries } from "./Model-Interfaces/TV/full-tv-series-interface";
+import { IMovie } from "./Model-Interfaces/MOVIE/movie-interface";
+import { ITV } from "./Model-Interfaces/TV/tv-series-interface";
+
+export interface MediaRepositoryInterface { //create interface that will contain the data we want to send to the client side. 
+    GetAllMovies(page?: number): Promise<IMovie | null>;
+    GetAllTVs(page?: number): Promise<ITV | null>;
+    GetAllMedia(page?: number): Promise<any>;
+    Search(query: string): Promise<any>;
+    GetMovieById(id: number): Promise<IFullMovie | null>;
+    GetTVById(id: number): Promise<IFullTVSeries | null>;
+}
