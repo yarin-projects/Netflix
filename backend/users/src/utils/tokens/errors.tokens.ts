78a943a170993082fcffba73c7ed66846362e948
yarin <yarin5445@gmail.com>
2025-03-21 17:52:20 +0200

    Add Error Handler Util In User Service

--- backend/users/src/utils/tokens/errors.tokens.ts
+++ backend/users/src/utils/tokens/errors.tokens.ts
@@ -2,4 +2,5 @@ export const errors = {
   jwtKeyMissing: 'JWT_KEY is not defined',
   mySqlConnectionFailed: 'MySql connection failed: ',
   mySqlVariablesMissing: 'DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST are required',
+  internalServerError: 'Internal server error',
 };
