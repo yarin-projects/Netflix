a7515f371e1558a8bcc97e6aac76250f4b8f1461
yarin <yarin5445@gmail.com>
2025-03-21 17:29:05 +0200

    Add Initial UserController To User Service

---
+++ backend/users/src/controllers/user.controller.ts
@@ -0,0 +1,15 @@
+import { injectable } from "inversify";
+
+@injectable()
+export class UserController {
+  constructor() {}
+  async singUp() {
+    throw new Error('Method not implemented.');
+  }
+  async login() {
+    throw new Error('Method not implemented.');
+  }
+  async logout() {
+    throw new Error('Method not implemented.');
+  }
+}
