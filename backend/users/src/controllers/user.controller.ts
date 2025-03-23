a10a3a5e03bb19e0f43f5a0652d51a8ac9cee1d6
yarin <yarin5445@gmail.com>
2025-03-23 19:04:37 +0200

    Add Implementation For UserController Methods In Users Service

--- backend/users/src/controllers/user.controller.ts
+++ backend/users/src/controllers/user.controller.ts
@@ -2,17 +2,45 @@ import { inject, injectable } from 'inversify';
 import { TOKENS } from '../utils/tokens.utils';
 import { IUserService } from '../interfaces/user-service.interface';
 import { Request, Response } from 'express';
+import { handleError } from '../utils/error-handler.utils';
+import { AuthRequestDto } from '../dtos/auth-request.dto';
+import { setCookie } from '../utils/cookies.utils';
 
 @injectable()
 export class UserController {
   constructor(@inject(TOKENS.injections.iUserService) private userService: IUserService) {}
   async singUp(req: Request, res: Response) {
-    throw new Error('Method not implemented.');
+    try {
+      const data: AuthRequestDto = req.body;
+
+      const token = await this.userService.signUp(data);
+
+      setCookie(res, token);
+
+      res
+        .status(TOKENS.httpStatus.CREATED)
+        .json({ message: TOKENS.messages.userCreated, email: data.email });
+    } catch (error) {
+      handleError(res, error);
+    }
   }
   async login(req: Request, res: Response) {
-    throw new Error('Method not implemented.');
+    try {
+      const data: AuthRequestDto = req.body;
+
+      const token = await this.userService.login(data);
+
+      setCookie(res, token);
+
+      res
+        .status(TOKENS.httpStatus.OK)
+        .json({ message: TOKENS.messages.loginSuccess, email: data.email });
+    } catch (error) {
+      handleError(res, error);
+    }
   }
   async logout(req: Request, res: Response) {
-    throw new Error('Method not implemented.');
+    res.clearCookie(TOKENS.token);
+    res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.logoutSuccess });
   }
 }
--- backend/users/src/utils/tokens/messages.tokens.ts
+++ backend/users/src/utils/tokens/messages.tokens.ts
@@ -1,4 +1,7 @@
 export const messages = {
   serverRunning: 'Server is running on port:',
   mySqlConncetionSuccess: 'MySql connection success',
+  userCreated: 'User created',
+  loginSuccess: 'Login success',
+  logoutSuccess: 'Logout success',
 };
