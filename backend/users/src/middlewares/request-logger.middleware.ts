2b42f26b0ba859e4c9d1475b72eed48d5d4df98e
yarin <yarin5445@gmail.com>
2025-03-25 20:19:28 +0200

    Add Api Response Interface To Change ResponseBody From Any Type, Refactor RequestLogger Middleware

--- backend/users/src/middlewares/request-logger.middleware.ts
+++ backend/users/src/middlewares/request-logger.middleware.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { logger } from '../configs/logger.config';
 import { hrtime } from 'node:process';
+import { ApiResponse } from '../interfaces/api-response.interface';
 
 export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   logger.info(`Incoming Request: [${req.method}] ${req.url}`);
   const start = hrtime.bigint();
 
-  let responseBody: any = null;
-  const originalJson = res.json.bind(res);
+  let responseBody: ApiResponse = {};
 
+  const originalJson = res.json.bind(res);
   res.json = (body: any) => {
     responseBody = body;
     return originalJson(body);
@@ -16,31 +17,37 @@ export const requestLogger = (req: Request, res: Response, next: NextFunction) =
 
   res.on('finish', () => {
     const end = hrtime.bigint();
-    const duration = (Number(end - start) / 1000000).toFixed(2);
+    const durationMs = (Number(end - start) / 1000000).toFixed(2);
 
-    let additionalProps = {};
-    if (responseBody && typeof responseBody === 'object') {
-      const { message: _, error: __, ...rest } = responseBody;
-      additionalProps = rest;
-    }
-
-    const additionalPropsString =
-      Object.keys(additionalProps).length > 0
-        ? ` - Additional Data: ${JSON.stringify(additionalProps)}`
-        : '';
+    const { message, error, additionalDataString } = extractResponseInfo(responseBody);
 
     if (res.statusCode >= 400) {
-      const { error } = responseBody;
       logger.error(
-        `Outgoing Response: [${res.statusCode}] ${req.url} (${duration} ms) - Error: ${error}${additionalPropsString}`
+        `Outgoing Response: [${res.statusCode}] ${req.url} (${durationMs} ms) - Error: ${error}${additionalDataString}`
       );
     } else {
-      const { message } = responseBody;
       logger.info(
-        `Outgoing Response: [${res.statusCode}] ${req.url} (${duration} ms) - Message: ${message}${additionalPropsString}`
+        `Outgoing Response: [${res.statusCode}] ${req.url} (${durationMs} ms) - Message: ${message}${additionalDataString}`
       );
     }
   });
 
   next();
+};
+
+const extractResponseInfo = (responseBody: ApiResponse) => {
+  if (!responseBody || typeof responseBody !== 'object') {
+    return { message: responseBody, error: undefined, additionalDataString: '' };
+  }
+  const { message, error, ...additionalData } = responseBody;
+  const hasAdditionalData = Object.keys(additionalData).length > 0;
+  const additionalDataString = hasAdditionalData
+    ? ` - Additional Data: ${JSON.stringify(additionalData)}`
+    : '';
+
+  return {
+    message,
+    error,
+    additionalDataString,
+  };
 };
