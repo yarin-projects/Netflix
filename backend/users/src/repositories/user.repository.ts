3c1033ff206c66fe01fc8b22b73aa523b260e2a2
yarin <yarin5445@gmail.com>
2025-03-26 20:21:33 +0200

    Add Implementations For RefreshToken Handling In Repository

--- backend/users/src/repositories/user.repository.ts
+++ backend/users/src/repositories/user.repository.ts
@@ -3,15 +3,33 @@ import { IUserRepository } from '../interfaces/user-repository.interface';
 import { IUser } from '../interfaces/user.interface';
 import { User } from '../models/user.model';
 import { AuthRequestDto } from '../dtos/auth-request.dto';
+import { IRefreshToken } from '../interfaces/refresh-token.interface';
+import { RefreshToken } from '../models/refresh-token.model';
 
 @injectable()
 export class UserRepository implements IUserRepository {
-  async create(data: AuthRequestDto): Promise<IUser | null> {
+  async createUser(data: AuthRequestDto): Promise<IUser | null> {
     const newUser = await User.create({ ...data });
     return newUser;
+  }
+  async createRefreshToken(data: IRefreshToken): Promise<IRefreshToken | null> {
+    const newRefreshToken = await RefreshToken.create({ ...data });
+    return newRefreshToken;
   }
   async findByEmail(email: string): Promise<IUser | null> {
     const user = await User.findOne({ where: { email } });
     return user;
+  }
+  async findRefreshToken(token: string, user_id: string): Promise<IRefreshToken | null> {
+    const refreshToken = await RefreshToken.findOne({ where: { token, user_id } });
+    return refreshToken;
+  }
+  async consumeRefreshToken(token: string, user_id: string): Promise<IRefreshToken | null> {
+    const refreshToken = await RefreshToken.findOne({ where: { token, user_id } });
+    if (refreshToken && !refreshToken.used) {
+      refreshToken.used = true;
+      await refreshToken.save();
+    }
+    return refreshToken;
   }
 }
