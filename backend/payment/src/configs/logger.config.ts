19c0ffcb6c88f22b459db0c97207c06d0902da46
yarin <yarin5445@gmail.com>
2025-03-26 11:00:55 +0200

    Add Logger Config

---
+++ backend/payment/src/configs/logger.config.ts
@@ -0,0 +1,29 @@
+import winston from 'winston';
+import path from 'path';
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const logMessageFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+const timestampFormat = timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' });
+
+const consoleLogFormatter = combine(colorize(), timestampFormat, logMessageFormat);
+const logFormatter = combine(timestampFormat, logMessageFormat);
+
+const relativeLogsPath = path.join(__dirname, '../../logs');
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: logFormatter,
+  transports: [
+    new winston.transports.Console({ format: consoleLogFormatter }),
+    new winston.transports.File({
+      filename: relativeLogsPath + '/payment-combined-logs.log',
+    }),
+    new winston.transports.File({
+      filename: relativeLogsPath + '/payment-error-logs.log',
+      level: 'error',
+    }),
+  ],
+});
