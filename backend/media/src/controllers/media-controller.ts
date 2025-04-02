c665b093bc72701b192eec8833617724b1313be1
yarin <yarin5445@gmail.com>
2025-04-02 18:45:57 +0300

    Fix Media Tests And Controller

--- backend/media/src/controllers/media-controller.ts
+++ backend/media/src/controllers/media-controller.ts
@@ -1,97 +1,89 @@
 import { inject, injectable } from 'inversify';
 import { IMediaService } from '../Interfaces/media-service-interface';
-import { TOKENS } from "../media-utils/tokens.utils";
+import { TOKENS } from '../media-utils/tokens.utils';
 import { Request, Response } from 'express';
-import { IFullMovie } from '../Interfaces/Model-Interfaces/MOVIE/full-movie-interface';
-import { IFullTVSeries } from '../Interfaces/Model-Interfaces/TV/full-tv-series-interface';
-import { ITVSeries } from '../Interfaces/Model-Interfaces/TV/tv-series-interface';
-import { IMovie } from '../Interfaces/Model-Interfaces/MOVIE/movie-interface';
 import { handleError } from '../media-utils/error-handler.utils';
 
 @injectable()
 export class MediaController {
-  constructor(@inject(TOKENS.injections.IMediaService) private mediaService: IMediaService) {} 
+  constructor(@inject(TOKENS.injections.IMediaService) private mediaService: IMediaService) {}
 
-    async GetAllMovies(req: Request, res: Response): Promise<IMovie[] | null> {
-        try {
-            const movies = await this.mediaService.GetAllMovies();
-            if (!movies) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMovies, data: movies });
-            return movies;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  async GetAllMovies(req: Request, res: Response) {
+    try {
+      const movies = await this.mediaService.GetAllMovies();
+      if (!movies) {
+        handleError(res, new Error(TOKENS.errors.movieNotFound));
+        return;
+      }
+      res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMovies, data: movies });
+    } catch (error) {
+      handleError(res, error);
     }
+  }
 
-    async GetAllTVs(req: Request, res: Response): Promise<ITVSeries[] | null> {
-        try {
-            const tvs = await this.mediaService.GetAllTVs();
-            if (!tvs) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getTVs, data: tvs });
-            return tvs;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  async GetAllTVs(req: Request, res: Response) {
+    try {
+      const tvs = await this.mediaService.GetAllTVs();
+      if (!tvs) {
+        handleError(res, new Error(TOKENS.errors.seriesNotFound));
+        return;
+      }
+      res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getTVs, data: tvs });
+    } catch (error) {
+      handleError(res, error);
     }
-    async GetAllMedia(req: Request, res: Response): Promise<any> {
-        try {
-            const allMedia = await this.mediaService.GetAllMedia();
-            if (!allMedia) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMedia, data: allMedia });
-            return allMedia;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  }
+  async GetAllMedia(req: Request, res: Response) {
+    try {
+      const allMedia = await this.mediaService.GetAllMedia();
+      if (!allMedia) {
+        handleError(res, new Error(TOKENS.errors.movieNotFound));
+        return;
+      }
+      res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMedia, data: allMedia });
+    } catch (error) {
+      handleError(res, error);
     }
+  }
 
-    async Search(req: Request, res: Response): Promise<any> {
-        try {
-            const searchMedia = await this.mediaService.Search(req.query.q as string); //idk about it 
-            if (!searchMedia) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.searchMedia, data: searchMedia });
-            return searchMedia;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  async Search(req: Request, res: Response) {
+    try {
+      const searchMedia = await this.mediaService.Search(req.query.q as string); //idk about it
+      if (!searchMedia) {
+        handleError(res, new Error(TOKENS.errors.mediaNotFound));
+        return;
+      }
+      res
+        .status(TOKENS.httpStatus.OK)
+        .json({ message: TOKENS.messages.searchMedia, data: searchMedia });
+    } catch (error) {
+      handleError(res, error);
     }
+  }
 
-    async GetMovieById(req: Request, res: Response): Promise<IFullMovie | null> {
-        try {
-            const movie = await this.mediaService.GetMovieById(req.body.id); //idk about it 
-            if (!movie) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMovieById, data: movie });
-            return movie;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  async GetMovieById(req: Request, res: Response) {
+    try {
+      const movie = await this.mediaService.GetMovieById(req.body.id); //idk about it
+      if (!movie) {
+        handleError(res, new Error(TOKENS.errors.movieNotFound));
+        return;
+      }
+      res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getMovieById, data: movie });
+    } catch (error) {
+      handleError(res, error);
     }
+  }
 
-    async GetTVById(req: Request, res: Response): Promise<IFullTVSeries | null> {
-        try {
-            const tv = await this.mediaService.GetTVById(req.body.id); //idk about it 
-            if (!tv) {
-                return null;
-            }
-            res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getTVById, data: tv });
-            return tv;
-        } catch (error) {
-            handleError(res,error);
-            return null;
-        }
+  async GetTVById(req: Request, res: Response) {
+    try {
+      const tv = await this.mediaService.GetTVById(req.body.id); //idk about it
+      if (!tv) {
+        handleError(res, new Error(TOKENS.errors.movieNotFound));
+        return;
+      }
+      res.status(TOKENS.httpStatus.OK).json({ message: TOKENS.messages.getTVById, data: tv });
+    } catch (error) {
+      handleError(res, error);
     }
+  }
 }
--- backend/media/src/media-utils/tokens/errors.tokens.ts
+++ backend/media/src/media-utils/tokens/errors.tokens.ts
@@ -2,5 +2,6 @@ export const errors = {
   internalServerError: 'Internal server error',
   movieNotFound: 'Movie not found',
   seriesNotFound: 'Series not found',
+  mediaNotFound: 'Media not found',
   jwtKeyMissing: 'JWT_KEY is not defined'
 };
