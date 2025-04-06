66a51455d34d61c138a11350d28224785cc09e5d
yarin <yarin5445@gmail.com>
2025-04-06 20:16:27 +0300

    Adjust Db Config And Connection In Users Service

--- backend/users/src/configs/mysql.config.ts
+++ backend/users/src/configs/mysql.config.ts
@@ -1,55 +1,47 @@
 import { Sequelize } from 'sequelize-typescript';
 import { TOKENS } from '../utils/tokens.utils';
-import { User } from '../models/user.model';
-import mysql from 'mysql2/promise';
 import { logger } from './logger.config';
+import { DbConfig } from '../interfaces/db-config.interface';
+import mysql from 'mysql2/promise';
 
 export class MySqlConnection {
   private static instance: Sequelize | null = null;
   private constructor() {}
-  public static async getInstance(
-    name: string,
-    user: string,
-    password: string,
-    host: string
-  ): Promise<Sequelize> {
+  public static async getInstance(dbConfig: DbConfig): Promise<Sequelize> {
     if (!MySqlConnection.instance) {
-      MySqlConnection.instance = await mySqlConnection(name, user, password, host);
+      MySqlConnection.instance = await this.connect(dbConfig);
     }
-    return MySqlConnection.instance;
+    return Promise.resolve(MySqlConnection.instance);
   }
+  private static connect = async (dbConfig: DbConfig) => {
+    const { name, user, password, host, dialect, ensureDbExists, runSync, models } = dbConfig;
+    if (ensureDbExists) {
+      await this.ensureDatabaseExists(dbConfig);
+    }
+    const sequelize = new Sequelize({
+      database: name,
+      username: user,
+      password,
+      host,
+      dialect: dialect,
+      logging: false,
+      models: models,
+    });
+    try {
+      await sequelize.authenticate();
+      logger.info(TOKENS.messages.mySqlConncetionSuccess);
+      console.log(TOKENS.messages.mySqlConncetionSuccess);
+      await sequelize.sync({ alter: runSync });
+      return sequelize;
+    } catch (error) {
+      logger.error(TOKENS.errors.mySqlConnectionFailed + error);
+      throw new Error(TOKENS.errors.mySqlConnectionFailed + error);
+    }
+  };
+  private static ensureDatabaseExists = async (dbConfig: DbConfig) => {
+    const { host, user, password, name: dbName } = dbConfig;
+    const connection = await mysql.createConnection({ host, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);
+    await connection.end();
+  };
 }
-
-const mySqlConnection = async (name: string, user: string, password: string, host: string) => {
-  await ensureDatabaseExists(host, user, password, name);
-  const sequelize = new Sequelize({
-    database: name,
-    username: user,
-    password,
-    host,
-    dialect: 'mysql',
-    logging: false,
-    models: [User],
-  });
-  try {
-    await sequelize.authenticate();
-    logger.info(TOKENS.messages.mySqlConncetionSuccess);
-    console.log(TOKENS.messages.mySqlConncetionSuccess);
-    await sequelize.sync({ alter: true });
-    return sequelize;
-  } catch (error) {
-    logger.error(TOKENS.errors.mySqlConnectionFailed + error);
-    throw new Error(TOKENS.errors.mySqlConnectionFailed + error);
-  }
-};
-
-const ensureDatabaseExists = async (
-  host: string,
-  user: string,
-  password: string,
-  dbName: string
-) => {
-  const connection = await mysql.createConnection({ host, user, password });
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);
-  await connection.end();
-};
