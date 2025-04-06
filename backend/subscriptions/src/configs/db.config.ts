0bd8ff1effdcdb29d611c6a9996875f1637ca72d
yarin <yarin5445@gmail.com>
2025-04-06 20:16:16 +0300

    Add Db Config And Connection To Subscriptions Service

---
+++ backend/subscriptions/src/configs/db.config.ts
@@ -0,0 +1,23 @@
+import { Dialect } from 'sequelize';
+import { DbConfig } from '../interfaces/db-config.interface';
+import { MySqlConnection } from './mysql.config';
+import { Subscription } from '../models/subscription.model';
+
+export const dbConnection = async () => {
+  const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SQL_DIALECT, NODE_ENV } = process.env;
+
+  const isProduction = NODE_ENV === 'production';
+
+  const dbConfig: DbConfig = {
+    name: DB_NAME!,
+    user: DB_USERNAME!,
+    password: DB_PASSWORD!,
+    host: DB_HOST!,
+    models: [Subscription],
+    dialect: (SQL_DIALECT as Dialect) || 'mysql',
+    ensureDbExists: !isProduction,
+    runSync: !isProduction,
+  };
+
+  return await MySqlConnection.getInstance(dbConfig);
+};
