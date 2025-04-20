1b11e8a97ac552ab18b57592bd29a4ba6c41ac97
yarin <yarin5445@gmail.com>
2025-04-20 22:56:45 +0300

    Change Default Zod Error In Auth Service

--- backend/auth/src/schemas/generate-token.schema.ts
+++ backend/auth/src/schemas/generate-token.schema.ts
@@ -3,15 +3,15 @@ import { TOKENS } from '../utils/tokens.utils';
 import { SubscriptionPlan } from '../enums/subscription-plan.enum';
 
 export const generateTokenSchema = z.object({
-  userId: z.string().uuid(TOKENS.validations.userId.message),
-  email: z.string().email(TOKENS.validations.email.message),
-  ipAddress: z.string().ip(TOKENS.validations.ipAddress.message),
+  userId: z.string({ required_error: TOKENS.validations.userId.required }).uuid(TOKENS.validations.userId.message),
+  email: z.string({ required_error: TOKENS.validations.email.required }).email(TOKENS.validations.email.message),
+  ipAddress: z.string({ required_error: TOKENS.validations.ipAddress.required }).ip(TOKENS.validations.ipAddress.message),
   userAgent: z
-    .string()
+    .string({ required_error: TOKENS.validations.userAgent.required })
     .min(TOKENS.validations.userAgent.min.value, TOKENS.validations.userAgent.min.message)
     .max(TOKENS.validations.userAgent.max.value, TOKENS.validations.userAgent.max.message),
   secret: z
-    .string()
+    .string({ required_error: TOKENS.validations.secret.required })
     .min(TOKENS.validations.secret.min.value, TOKENS.validations.secret.min.message)
     .max(TOKENS.validations.secret.max.value, TOKENS.validations.secret.max.message),
   subscriptionPlan: z.nativeEnum(SubscriptionPlan).optional().nullable(),
