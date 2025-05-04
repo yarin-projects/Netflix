d85c88f83dbd92cb0101c1d5a87704612ec03631
yarin <yarin5445@gmail.com>
2025-05-05 01:39:48 +0300

    Finish Dependency Injections In Profile Service

---
+++ backend/profiles/src/configs/inversify.config.ts
@@ -0,0 +1,26 @@
+import { Container } from 'inversify';
+import { ProfileController } from '../controllers/profile.controller';
+import { InternalProfileController } from '../controllers/internal-profile.controller';
+import { TOKENS } from '../utils/tokens.utils';
+import { IProfileService } from '../interfaces/profile-service.interface';
+import { ProfileService } from '../services/profile.service';
+import { IInternalProfileService } from '../interfaces/internal-profile-service.interface';
+import { InternalProfileService } from '../services/internal-profile.service';
+import { IProfileRepository } from '../interfaces/profile-repository.interface';
+import { ProfileRepository } from '../repositories/profile.repository';
+
+const container = new Container();
+
+container.bind<ProfileController>(TOKENS.injections.profileController).to(ProfileController);
+container
+  .bind<InternalProfileController>(TOKENS.injections.internalProfileController)
+  .to(InternalProfileController);
+
+container.bind<IProfileService>(TOKENS.injections.iProfileService).to(ProfileService);
+container
+  .bind<IInternalProfileService>(TOKENS.injections.iInternalProfileService)
+  .to(InternalProfileService);
+
+container.bind<IProfileRepository>(TOKENS.injections.iProfileService).to(ProfileRepository);
+
+export { container };
